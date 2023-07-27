ebfa99e6a278500e98b5985ff4faf99cd3f1341b
Julia <jula77@inbox.ru>
2023-07-28 00:50:07 +0300

    My summary

---
+++ build/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.lambdaHandler = void 0;
+const lambdaHandler = async (event) => {
+    const queries = JSON.stringify(event.queryStringParameters);
+    return {
+        statusCode: 200,
+        body: `Queries: ${queries}`
+    };
+};
+exports.lambdaHandler = lambdaHandler;
+console.log('Hello world!');
+//# sourceMappingURL=index.js.map
